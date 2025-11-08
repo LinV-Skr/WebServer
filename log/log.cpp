969333d3decb91666f9ea959c0a320b9af66c20c
LinV-Skr <wzlCssc@163.com>
2025-11-08 22:40:35 +0800

    ChangeLog

--- log/log.cpp
+++ log/log.cpp
@@ -4,15 +4,24 @@ Log::Log()
 {
     m_count = 0;
     m_is_async = false;
+    m_block_queue = nullptr;
+    m_fp = nullptr;
 }
 
 Log::~Log()
 {
     delete m_block_queue;
     m_block_queue = nullptr;
+
+    if(nullptr != m_fp)
+    {
+        fclose(m_fp);
+        delete m_fp;
+        m_fp = nullptr;
+    }
 }
 
-bool Log::Init(const char * file_name, int close_log, int log_buff_size, int split_lines, int max_queue_size)
+bool Log::Init(const char * file_name, int close_log, int log_buf_size, int split_lines, int max_queue_size)
 {
     //  如果设置了max_queue_size，则设为异步
     if(1 <= max_queue_size)
@@ -22,8 +31,26 @@ bool Log::Init(const char * file_name, int close_log, int log_buff_size, int spl
         //  创建线程
         pthread_t tid;
         //  TODO: 线程回收
-        //  TODO: 回调函数
         pthread_create(&tid, NULL, Flush_Log_Thread, NULL);
     }
+
+    m_close_log = close_log;
+    m_log_buff_size = log_buf_size;
+    m_log_buf = new char[log_buf_size];
+    memset(m_log_buf, '\0', m_log_buff_size);
+    m_split_lines = split_lines;
+
+    time_t t_time = time(NULL);
+    struct tm t_tm;
+    //  localtime函数存在线程安全问题
+    localtime_r(&t_time, &t_tm);
+
+    const char * p_file_name = strrchr(file_name, '/');
+    char log_file_name[256] = {0};
+    if(NULL == p_file_name)
+    {
+        snprintf(log_file_name, 255, "%d_%2d_%2d_%s", t_tm.tm_year + 1900, t_tm.tm_mon, t_tm.tm_mday, file_name);
+    }
+
     return true;
 }
--- log/log.h
+++ log/log.h
@@ -2,7 +2,9 @@
 #define LOG_H
 
 #include <string>
+#include <string.h>
 #include <pthread.h>
+#include <stdio.h>
 
 #include "./block_queue.h"
 
@@ -27,14 +29,28 @@ public:
     Log& operator=(const Log &) = delete;
 
     //  异步线程回调函数
-    void * Flush_Log_Thread(void * arg)
+    static void * Flush_Log_Thread(void * arg)
     {
-        return nullptr;
+        Log::GetInstance().Async_Write_Log();
     }
 
     //  初始化函数
     bool Init(const char * file_name, int close_log, int log_buff_size = 8192,
                 int split_lines = 5000000, int max_queue_size = 0);
+
+private:
+    void Async_Write_Log()
+    {
+        string single_log;
+        while(m_block_queue->Pop(single_log))
+        {
+            m_mutex.lock();
+            if(nullptr != m_fp)
+            {
+                fputs(single_log.c_str(), m_fp);
+            }
+        }
+    }
 
 private:
     //  日志行数记录
@@ -45,6 +61,24 @@ private:
 
     //  日志阻塞队列
     BlockQueue<string> * m_block_queue;
+
+    //  互斥量
+    locker m_mutex;
+
+    //  日志文件
+    FILE * m_fp;
+
+    //  是否关闭日志
+    int m_close_log;
+
+    //  日志缓冲区大小
+    int m_log_buff_size;
+
+    //  日志缓冲区
+    char * m_log_buf;
+
+    //  日志最大行数
+    int m_split_lines;
 };
 
 #endif
